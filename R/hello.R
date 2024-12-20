37c751f7bd3973f7b1c0a4c073317616f679e4c9
stemterminalgutter <jennwatk@iu.edu>
2024-12-20 15:11:55 -0500

    updated documentation, r code

--- R/hello.R
+++ R/hello.R
@@ -1,4 +1,4 @@
-huge_word_list <- readLines("Your/Path/Here/words_alpha.txt")
+huge_word_list <- readLines("~/Library/CloudStorage/OneDrive-IndianaUniversity/Getting Started in R/Final Biostats Project/words_alpha.txt")
 
 file.path(getwd(), "./words_alpha.txt")
 #' Random Word Generator
@@ -6,7 +6,7 @@ file.path(getwd(), "./words_alpha.txt")
 #' @param n The random word
 #' @param huge_word_list
 #'
-#' @returns The output from \code{\link{print}}
+#' @return The output from \code{\link{print}}
 #' @export
 #'
 #' @examples
@@ -18,18 +18,25 @@ random_words <- generate_random_words(1, huge_word_list)
 print(random_words)
 
 
-text <- readLines("Your/Path/Here/crime_and_punishment.txt")
+text <- readLines("~/Library/CloudStorage/OneDrive-IndianaUniversity/Getting Started in R/Final Biostats Project/crime_and_punishment.txt")
 
 file.path(getwd(), "./crime_and_punishment.txt")
-
+library(tm)
+library(wordcloud)
+library(ngram)
+library(RColorBrewer)
 
 #' Wordcloud That Displays the Most Frequently Used Word
 #'
 #' @param x The input string we want to process, like removing punctuation
 #'
-#' @returns The output from \code{\link{wordcloud()}}
+#' @return The output from \code{\link{wordcloud()}}
 #' @export
 #'
+#' @importFrom tm removeWords stopwords
+#' @import wordcloud
+#' @importFrom ngram get.phrasetable
+#' @import RColorBrewer
 #' @examples
 remove_custom_punctuation <- function(x) {
   x <- gsub("'", "", x)  # Remove apostrophes
@@ -63,16 +70,15 @@ combined_freq_table <- rbind(freq_table_1gram, freq_table_2gram)
 # Create the word cloud
 wordcloud(words = combined_freq_table$ngrams,
           freq = combined_freq_table$freq,
-          scale = c(5, 0.3),
-          max.words = 150,
+          scale = c(3, 0.7),
+          max.words = 100,
           random.order = FALSE,
           rot.per = 0.35,
-          colors = brewer.pal(8, "Paired"),
-          family= "serif")
+          colors = brewer.pal(8, "Paired"))
 
 #' Fun Game Using Words from Random Word Generator
 #'
-#' @returns The output from \code{\link{play_word_game}}
+#' @return The output from \code{\link{play_word_game}}
 #' @export
 #'
 #' @examples
